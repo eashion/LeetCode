5ec6dfb95dd61050d23a37c405cfe0d35a2d5bed
eashion <eashion1994@gmail.com>
2016-12-20 09:52:50 +0800

    Create MySQL.sql

---
+++ 176_Second Highest Salary/MySQL.sql
@@ -0,0 +1,7 @@
+select (
+  select distinct Salary from Employee order by Salary Desc limit 1 offset 1
+)as SecondHighestSalary
+
+SELECT max(Salary) as SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee)
