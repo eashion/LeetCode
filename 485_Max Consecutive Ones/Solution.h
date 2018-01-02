dbc48eae1977ad1230cf82dae97a03a37a022b33
eashion <eashion1994@gmail.com>
2018-01-01 19:41:03 -0800

    Update Solution.h

--- 485_Max Consecutive Ones/Solution.h
+++ 485_Max Consecutive Ones/Solution.h
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int res = nums[0]==1?1:0;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] == 1) {
                 nums[i] = nums[i-1]+1;
             } else {
-                res = max(res,nums[i-1]);
+                nums[0] = max(nums[0],nums[i-1]);
             }
         }
-        res = max(res,nums[nums.size()-1]);
-        return res;
+        nums[0] = max(nums[0],nums[nums.size()-1]);
+        return nums[0];
     }
 };
