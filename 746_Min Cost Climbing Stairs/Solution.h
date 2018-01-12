daaf71608a0c9f5c76f70d513e116c6cb5869f98
eashion <eashion1994@gmail.com>
2018-01-11 23:31:55 -0800

    Create Solution.h

---
+++ 746_Min Cost Climbing Stairs/Solution.h
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size()+1);
+        dp[0] = 0;
+        dp[1] = cost[0];
+        for(int i = 2; i <= cost.size(); i++){
+            dp[i] = min(dp[i-1],dp[i-2])+cost[i-1];
+        }
+        return min(dp[cost.size()],dp[cost.size()-1]);
+    }
+};
