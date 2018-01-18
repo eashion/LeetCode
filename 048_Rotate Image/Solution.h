b0e6e462528dee8d4b020cfcf66e623263bfbb08
eashion <eashion1994@gmail.com>
2018-01-17 21:53:06 -0800

    Create Solution.h

---
+++ 048_Rotate Image/Solution.h
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if (matrix.size()<=1) {
+            return ;
+        }
+        int len = matrix.size();
+        for(int i = 0; i < len/2; i++){
+            for(int j = i; j < i+(len-2*i)-1; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[len-1-j][i];
+                matrix[len-1-j][i] = matrix[len-1-i][len-1-j];
+                matrix[len-1-i][len-1-j] = matrix[j][len-1-i];
+                matrix[j][len-1-i] = tmp;
+            }
+        }
+        return ;
+    }
+};
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
+void anti_rotate(vector<vector<int> > &matrix) {
+    for (auto vi : matrix) reverse(vi.begin(), vi.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
