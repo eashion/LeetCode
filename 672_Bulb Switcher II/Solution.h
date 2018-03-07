cc2dcb0eea4bd65a64572eb498d17a45bbfd3a7d
eashion <eashion1994@gmail.com>
2018-03-06 23:49:04 -0800

    Create Solution.h

---
+++ 672_Bulb Switcher II/Solution.h
@@ -0,0 +1,14 @@
+#the great solution in discuss is same as mine, but better.
+#if n>3 && m>4, we can conclude that the answer is 8.
+class Solution {
+    public int flipLights(int n, int m) {
+        if(m==0) return 1;
+        if(n==1) return 2;
+        if(n==2&&m==1) return 3;
+        if(n==2) return 4;
+        if(m==1) return 4;
+        if(m==2) return 7;
+        if(m>=3) return 8;
+        return 8;
+    }
+}
